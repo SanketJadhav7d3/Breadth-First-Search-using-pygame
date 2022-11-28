842214530836832dd87da839a73d62c5c7333bc2
SanketJadhav7d3 <lonewolf7d3@gmail.com>
2022-11-28 22:03:15 +0530

    Implement again from scratch

--- bfs.py
+++ bfs.py
@@ -1,123 +1,129 @@
 
-from PathFinding.window import Window
 import pygame
-from pygame.locals import *
 from queue import Queue
-import time 
+from pygame.locals import *
 import numpy as np
 
-
-class BFS(Window):
+class BFS:
 
     def __init__(self):
-        super().__init__()
-        self.frontier = Queue()
-        self.came_from = dict()
-        self.clock = pygame.time.Clock()
+        pygame.init()
+        self.height = 800
+        self.width = 800
+        self.tile = 10
+        self.graph = np.zeros((self.width // self.tile, self.height // self.tile))
+        self.window = pygame.display.set_mode((self.width, self.height))
+        self.start_node = None
+        self.target_node = None
+        self.visited = {}       # to keep track of visited elements
+        self.queue = Queue()    # will keep adjacent elements 
+
+    def draw_node(self, x, y, color):
+        '''
+            Draws node given mouse x and y coordinates
+        '''
+        s = self.tile
+        node = pygame.Rect(x*s, y*s, s-1, s-1);
+        pygame.draw.rect(self.window, color, node, border_radius=3)
+
+    def get_grid_pos(self):
+        x, y = pygame.mouse.get_pos()
+
+        x //= self.tile
+        y //= self.tile
+
+        return x, y
+
+    def draw_graph(self):
+
+        for i in range(self.graph.shape[0]):
+            for j in range(self.graph.shape[1]):
+                if self.graph[i][j] == 1:
+                    self.draw_node(i, j, (255, 0, 0))
+
+                if self.graph[i][j] == 2:
+                    self.draw_node(i, j, (255, 255, 0))
+
+    def get_neighbours(self, node):
+        
+        neighbours = []
+        x, y = node
+
+        if x + 1 < self.graph.shape[0]: neighbours.append((x+1, y))
+        if x - 1 >= 0: neighbours.append((x-1, y))
+
+        if y + 1 < self.graph.shape[1]: neighbours.append((x, y+1))
+        if y - 1 >= 0: neighbours.append((x, y-1))
+
+        return neighbours
 
     def flood(self):
-        if self.start_flag == None or self.end_flag == None:
-            print("Start flag not initialized")
-            return
-
-        self.frontier.put(self.start_flag)
-        self.came_from[self.start_flag] = None
+        self.queue.put(self.start_node)
+        self.visited[self.start_node] = None
         
-        while not self.frontier.empty():
-            current = self.frontier.get()
+        while not self.queue.empty():
+            current = self.queue.get()
 
             x, y = current
-            if self.nodes[x][y] == 1:
-                continue
-    
-            for n in self.get_neighbours(current):
-                if n not in self.came_from:
-                    self.frontier.put(n)
-                    self.came_from[n] = current
 
-        current = self.end_flag
+            if self.graph[x][y] == 1:
+                continue
+
+            for n in self.get_neighbours(current):
+
+                if n not in self.visited:
+                    self.queue.put(n)
+                    self.visited[n] = current
+
+        current = self.target_node
         path = []
 
-        while current != self.start_flag:
+        while current != self.start_node:
             path.append(current)
-            current = self.came_from[current]
-            
-        path.append(self.start_flag)
+            current = self.visited[current]
+
+        path.append(self.start_node)
+
         for p in path:
             x, y = p
-            self.nodes[x][y] = 2
+            self.graph[x][y] = 2
 
-    def show(self):
-        '''
-            Displays all the nodes
-        '''
-        s = self.node_size
-        for i in range(self.nodes.shape[0]):
-            for j in range(self.nodes.shape[1]):
-                node = pygame.Rect(i * s, j * s, s - 1, s - 1)
-                if self.nodes[i][j] == 0:
-                    self.draw_node(i, j, (32, 37, 40))
-                if self.nodes[i][j] == 1:
-                    self.draw_node(i, j, (104, 127, 145))
-                if self.nodes[i][j] == 2:
-                    self.draw_node(i, j, (229, 229, 227))
 
-        # draw the start and end node
-        if self.start_flag is not None:
-            s_x, s_y = self.start_flag
-            self.draw_node(s_x, s_y, (0, 255, 255))
-
-        # draw the start and end node
-        if self.end_flag is not None:
-            e_x, e_y = self.end_flag
-            self.draw_node(e_x, e_y, (164, 147, 146))
-
-        # render help messages
-        self.render_messages()
-
-    # override run method
     def run(self):
+
+        running = True
         drawing = False
-        while True:
+
+        while running:
+
             for event in pygame.event.get():
+
                 if event.type == pygame.QUIT:
-                    exit()
+                    running = False
 
                 if event.type == pygame.MOUSEMOTION:
 
                     if drawing:
                         x, y = self.get_grid_pos()
-                        self.nodes[x][y] = 1
+                        self.graph[x][y] = 1
 
                 if event.type == pygame.KEYDOWN:
 
-                    if event.key == K_RETURN:
-                        print(self.start_flag, self.end_flag)
-
-                    if event.key == K_c:
-                        # clear the screen
-                        self.nodes = np.zeros((self.rows, self.cols))
-                        self.start_flag = None
-                        self.end_flag = None
-                        self.frontier = Queue()
-                        self.came_from = dict()
-
                     if event.key == K_f:
                         self.flood()
-
-                    if event.key == K_p:
-                        print(self.start_flag, self.end_flag)
-
+                
                 if event.type == pygame.MOUSEBUTTONDOWN:
 
-                    if self.start_flag == None:
-                        x, y = self.get_grid_pos()
-                        self.start_flag = x, y
+                    if self.start_node == None:
+                        self.start_node = self.get_grid_pos()
+                        x, y = self.start_node
+                        self.graph[x][y] = 2
                         continue
 
-                    if self.end_flag == None and self.start_flag != None:
-                        x, y = self.get_grid_pos()
-                        self.end_flag = x, y
+                    if self.target_node == None and self.start_node is not None:
+                        self.target_node = self.get_grid_pos()
+                        x, y = self.target_node
+                        self.graph[x][y] = 2
                         continue
 
                     drawing = True
@@ -125,13 +131,15 @@ class BFS(Window):
                 if event.type == pygame.MOUSEBUTTONUP:
                     drawing = False
 
-                self.show()
-                pygame.display.update()
+            for node in self.visited:
+                self.draw_node(node[0], node[1], (0, 0, 255))
+
+            self.draw_graph()
+            pygame.display.update()
 
         pygame.quit()
 
 
 if __name__ == "__main__":
     b = BFS()
-
     b.run()
